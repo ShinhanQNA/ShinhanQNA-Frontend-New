b59eb0ce25114956bd49ad4044609b747355149f
김도훈 <dohun0310@naver.com>
2026-04-11 20:02:51 +0900

    [Design/#7] Design: 홈 피드 Skeleton 로딩 UI 추가

---
+++ apps/home/app/(main)/loading.tsx
@@ -0,0 +1,24 @@
+import { Skeleton } from "@shinhanqna/ui";
+
+export default function MainLoading() {
+  return (
+    <div className="flex flex-col">
+      <div className="flex border-b border-gray-200 px-2">
+        {Array.from({ length: 4 }).map((_, i) => (
+          <Skeleton key={i} className="h-10 w-20 mx-2 my-1 rounded-md" />
+        ))}
+      </div>
+      {Array.from({ length: 5 }).map((_, i) => (
+        <div key={i} className="flex gap-3 px-4 py-3 border-b border-gray-200">
+          <Skeleton className="h-10 w-10 rounded-full shrink-0" />
+          <div className="flex-1 flex flex-col gap-2">
+            <Skeleton className="h-4 w-32" />
+            <Skeleton className="h-5 w-3/4" />
+            <Skeleton className="h-4 w-full" />
+            <Skeleton className="h-4 w-1/2" />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
