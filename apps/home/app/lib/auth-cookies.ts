9554b7b996d7054557af5c98ce786d0fabf0e96a
김도훈 <dohun0310@naver.com>
2026-04-13 18:04:38 +0900

    [Fix/#25] Fix: 홈 앱 인증 쿠키 이름을 student_* 로 분리

--- apps/home/app/lib/auth-cookies.ts
+++ apps/home/app/lib/auth-cookies.ts
@@ -1,7 +1,7 @@
 import { cookies } from "next/headers";
 
-const ACCESS_TOKEN_KEY = "access_token";
-const REFRESH_TOKEN_KEY = "refresh_token";
+const ACCESS_TOKEN_KEY = "student_access_token";
+const REFRESH_TOKEN_KEY = "student_refresh_token";
 
 const COOKIE_OPTIONS = {
   httpOnly: true,
